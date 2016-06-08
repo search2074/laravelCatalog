5622c273dac4f7171cd1c1a0ac6d3d366979673f
search2074 <chelbmk@mail.ru>
2016-06-08 09:31:54 +0400

    Изменил настройку отступов

--- database/migrations/2016_06_07_173444_create_cities_table.php
+++ database/migrations/2016_06_07_173444_create_cities_table.php
@@ -5,28 +5,28 @@ use Illuminate\Database\Migrations\Migration;
 
 class CreateCitiesTable extends Migration
 {
-    /**
-     * Run the migrations.
-     *
-     * @return void
-     */
-    public function up()
-    {
-        Schema::create('cities', function (Blueprint $table) {
-            $table->increments('id');
-			//добавим дату создания и изменения
-            $table->timestamps();
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('cities', function (Blueprint $table) {
+			$table->increments('id');
+			//добавим дату создания и модицикации
+			$table->timestamps();
 			$table->string('name');
-        });
-    }
+		});
+	}
 
-    /**
-     * Reverse the migrations.
-     *
-     * @return void
-     */
-    public function down()
-    {
-        Schema::drop('cities');
-    }
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('cities');
+	}
 }
